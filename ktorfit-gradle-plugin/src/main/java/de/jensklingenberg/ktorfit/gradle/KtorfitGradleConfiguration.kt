f66a5dd9e4225daa0e027b6b97c3177159aa1e2b
Jens Klingenberg <mail@jensklingenberg.de>
2023-10-24 23:21:48 +0200

    refactor code generation

--- ktorfit-gradle-plugin/src/main/java/de/jensklingenberg/ktorfit/gradle/KtorfitGradleConfiguration.kt
+++ ktorfit-gradle-plugin/src/main/java/de/jensklingenberg/ktorfit/gradle/KtorfitGradleConfiguration.kt
@@ -9,8 +9,7 @@ open class KtorfitGradleConfiguration {
     /**
      * version number of the compiler plugin
      */
-    @Deprecated("Update the Gradle plugin instead of updating this version")
-    var version: String = "1.9.1" // remember to bump this version before any release!
+    internal var version: String = "2.0.0-SNAPSHOT" // remember to bump this version before any release!
 
     /**
      * used to get debug information from the compiler plugin
